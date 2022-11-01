5943c6329ee793863142e6ac0d777a6d8d2f4a7c
RaghavC16 <50890157+RaghavC16@users.noreply.github.com>
2022-11-01 11:05:22 -0500

    started main

--- app/src/main/java/com/example/spotifyquizproject/MainActivity.java
+++ app/src/main/java/com/example/spotifyquizproject/MainActivity.java
@@ -11,4 +11,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // We will start writing our code here.
+    }
+
+    private void connected() {
+        // Then we will write some more code here.
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Aaand we will finish off here.
+    }
 }
