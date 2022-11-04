d2f7036db33044482985faa674656fdabaf80379
RaghavC16 <50890157+RaghavC16@users.noreply.github.com>
2022-11-04 11:00:50 -0500

    added import url functionality

--- app/src/main/java/com/example/spotifyquizproject/ImportURL.java
+++ app/src/main/java/com/example/spotifyquizproject/ImportURL.java
@@ -4,15 +4,100 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.spotify.android.appremote.api.ConnectionParams;
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+import com.spotify.protocol.types.Track;
 
 public class ImportURL extends AppCompatActivity {
+
+    private static final String CLIENT_ID = "0b6a257c37744cfabe83c6949f68019f";
+    private static final String REDIRECT_URI = "http://localhost:8888/callback";
+    private SpotifyAppRemote mSpotifyAppRemote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_importurl);
 
-        Intent intent = new Intent(ImportURL.this, PlayQuiz.class);
-        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ConnectionParams connectionParams =
+                new ConnectionParams.Builder(CLIENT_ID)
+                        .setRedirectUri(REDIRECT_URI)
+                        .showAuthView(true)
+                        .build();
+
+        SpotifyAppRemote.connect(this, connectionParams,
+                new Connector.ConnectionListener() {
+
+                    @Override
+                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+                        mSpotifyAppRemote = spotifyAppRemote;
+                        Log.d("MainActivity", "Connected! Yay!");
+
+                        // Now you can start interacting with App Remote
+                        //connected();
+
+                    }
+
+                    @Override
+                    public void onFailure(Throwable throwable) {
+                        Log.e("MyActivity", throwable.getMessage(), throwable);
+
+                        // Something went wrong when attempting to connect! Handle errors here
+                    }
+                });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+    }
+
+    private void connected(String uri) {
+        // Play a playlist
+        mSpotifyAppRemote.getPlayerApi().play(uri);
+
+        // Subscribe to PlayerState
+        mSpotifyAppRemote.getPlayerApi()
+                .subscribeToPlayerState()
+                .setEventCallback(playerState -> {
+                    final Track track = playerState.track;
+                    if (track != null) {
+                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+                    }
+                });
+    }
+
+
+
+    public void playPlaylist(View view){
+        EditText editText = findViewById(R.id.playlistLink);
+
+        String text = editText.getText().toString();
+
+        String playlist = "playlist";
+
+        int index = text.indexOf("playlist");
+        
+
+
+        connected(uri);
+
+    }
+
+
+    public void switchScreens1(View view){
+        startActivity(new Intent(getApplicationContext(), PlayQuiz.class));
     }
 }
--- app/src/main/java/com/example/spotifyquizproject/MainActivity.java
+++ app/src/main/java/com/example/spotifyquizproject/MainActivity.java
@@ -7,6 +7,7 @@ package com.example.spotifyquizproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -35,66 +36,84 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ConnectionParams connectionParams =
-                new ConnectionParams.Builder(CLIENT_ID)
-                        .setRedirectUri(REDIRECT_URI)
-                        .showAuthView(true)
-                        .build();
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        ConnectionParams connectionParams =
+//                new ConnectionParams.Builder(CLIENT_ID)
+//                        .setRedirectUri(REDIRECT_URI)
+//                        .showAuthView(true)
+//                        .build();
+//
+//        SpotifyAppRemote.connect(this, connectionParams,
+//                new Connector.ConnectionListener() {
+//
+//                    @Override
+//                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
+//                        mSpotifyAppRemote = spotifyAppRemote;
+//                        Log.d("MainActivity", "Connected! Yay!");
+//
+//                        // Now you can start interacting with App Remote
+//                        //connected();
+//
+//                    }
+//
+//                    @Override
+//                    public void onFailure(Throwable throwable) {
+//                        Log.e("MyActivity", throwable.getMessage(), throwable);
+//
+//                        // Something went wrong when attempting to connect! Handle errors here
+//                    }
+//                });
+//    }
+//
+//    @Override
+//    protected void onStop() {
+//        super.onStop();
+//        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
+//    }
+//
+//    private void connected(String uri) {
+//        // Play a playlist
+//        mSpotifyAppRemote.getPlayerApi().play(uri);
+//
+//        // Subscribe to PlayerState
+//        mSpotifyAppRemote.getPlayerApi()
+//                .subscribeToPlayerState()
+//                .setEventCallback(playerState -> {
+//                    final Track track = playerState.track;
+//                    if (track != null) {
+//                        Log.d("MainActivity", track.name + " by " + track.artist.name);
+//                    }
+//                });
+//    }
+//
+//
+//
+//    public void playPlaylist(View view){
+//        EditText editText = findViewById(R.id.playlistLink);
+//
+//        String uri = editText.getText().toString();
+//        connected(uri);
+//
+//    }
+    public void changeToUrl(View view){
 
-        SpotifyAppRemote.connect(this, connectionParams,
-                new Connector.ConnectionListener() {
+        // get a reference to the EditText element in the xml page that is connected to this activity.
+        // to this activity
+        // extract the text
 
-                    @Override
-                    public void onConnected(SpotifyAppRemote spotifyAppRemote) {
-                        mSpotifyAppRemote = spotifyAppRemote;
-                        Log.d("MainActivity", "Connected! Yay!");
 
-                        // Now you can start interacting with App Remote
-                        //connected();
+        /*
+        Create an intent (envelope) by telling Android who the sender and receiver is
+        then we need to fill teh intent and lastly we need to start the intent.
+         */
 
-                    }
+        Intent intent = new Intent(this, ImportURL.class);
 
-                    @Override
-                    public void onFailure(Throwable throwable) {
-                        Log.e("MyActivity", throwable.getMessage(), throwable);
+        startActivity(intent);
 
-                        // Something went wrong when attempting to connect! Handle errors here
-                    }
-                });
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
-    }
-
-    private void connected(String uri) {
-        // Play a playlist
-        mSpotifyAppRemote.getPlayerApi().play(uri);
-
-        // Subscribe to PlayerState
-        mSpotifyAppRemote.getPlayerApi()
-                .subscribeToPlayerState()
-                .setEventCallback(playerState -> {
-                    final Track track = playerState.track;
-                    if (track != null) {
-                        Log.d("MainActivity", track.name + " by " + track.artist.name);
-                    }
-                });
-    }
-
-
-
-    public void playPlaylist(){
-        EditText editText = findViewById(R.id.playlistLink);
-
-        String uri = editText.getText().toString();
-        connected(uri);
-
-    }
-
+//
 }
