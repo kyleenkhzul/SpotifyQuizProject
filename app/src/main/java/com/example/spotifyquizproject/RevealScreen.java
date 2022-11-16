d9d12f65de4c2a9dbb44cebceb6fd1c04dc4ac81
RaghavC16 <50890157+RaghavC16@users.noreply.github.com>
2022-11-16 10:52:21 -0600

    add points functionality working

--- app/src/main/java/com/example/spotifyquizproject/RevealScreen.java
+++ app/src/main/java/com/example/spotifyquizproject/RevealScreen.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RevealScreen extends AppCompatActivity {
 
@@ -15,16 +18,52 @@ public class RevealScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reveal_screen);
 
-        Intent intent = new Intent(RevealScreen.this, GameScreen.class);
-        startActivity(intent);
 
-        Intent intent2 = new Intent(RevealScreen.this, EndScreen.class);
-        startActivity(intent2);
     }
 
-    public void updatePlayerOne(){
-        playerOnePoints++;
+    public void updatePlayerOne(View v){
+        TextView textView = findViewById(R.id.points1);
+        playerOnePoints = Integer.parseInt(textView.getText().toString());
+
+        switch(v.getId()){
+            case R.id.playerOneIncrement:
+                playerOnePoints++;
+                break;
+            case R.id.playerOneDecrement:
+                playerOnePoints--;
+                break;
+        }
+
+        textView.setText(Integer.toString(playerOnePoints));
     }
+
+
+    public void updatePlayerTwo(View v){
+        TextView textView = findViewById(R.id.points2);
+        playerTwoPoints = Integer.parseInt(textView.getText().toString());
+
+        switch(v.getId()){
+            case R.id.playerTwoIncrement:
+                playerTwoPoints++;
+                break;
+            case R.id.playerTwoDecrement:
+                playerTwoPoints--;
+                break;
+        }
+
+        textView.setText(Integer.toString(playerTwoPoints));
+    }
+
+
+    public void switchBackToGame(View view){
+        startActivity(new Intent(getApplicationContext(), GameScreen.class));
+    }
+
+    public void switchToEnd(View view){
+        startActivity(new Intent(getApplicationContext(), EndScreen.class));
+    }
+
+
 
 
 }
