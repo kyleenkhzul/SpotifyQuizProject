7c36dd91b62fb2a83981b202d68d6fc2acc5f009
SilentWatch225 <90773123+SilentWatch225@users.noreply.github.com>
2022-11-30 10:55:42 -0600

    End Screen finished and all variables should set normally

--- app/src/main/java/com/example/spotifyquizproject/EndScreen.java
+++ app/src/main/java/com/example/spotifyquizproject/EndScreen.java
@@ -5,17 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class EndScreen extends AppCompatActivity {
+    TextView play;
+    TextView point;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_screen);
+        //double check this, source of error
         GameScreen.compareValues(MainActivity.spotifyHelper.getP1Points(), MainActivity.spotifyHelper.getP1Points());
+        play = findViewById(R.id.playerNumber);
+        point = findViewById(R.id.pointValue);
+        values();
     }
 
     public void switchToStart(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void values(){
+        if(MainActivity.spotifyHelper.getP1Points() > MainActivity.spotifyHelper.getP2Points()){
+            play.setText("Player 1");
+            point.setText(MainActivity.spotifyHelper.getP1Points() + " Points!");
+        }else if(MainActivity.spotifyHelper.getP1Points() < MainActivity.spotifyHelper.getP2Points()){
+            play.setText("Player 2");
+            point.setText(MainActivity.spotifyHelper.getP2Points() + " Points!");
+        }else{
+            play.setText("Everybody");
+            point.setText(MainActivity.spotifyHelper.getP1Points() + " Points!");
+        }
     }
 }
